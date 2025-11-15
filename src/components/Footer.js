be2c02ceccc461d7211ec0c8ce065f041a648bff
Manni <150657478+Manni-Liu@users.noreply.github.com>
2025-11-15 13:08:19 +1300

    Update Footer
    Modify Footer diaplay

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.png';
+import navIcon3 from '../assets/img/nav-icon3.png';
 
 export const Footer = () => {
   return (
@@ -10,11 +10,11 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
 
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={12} className="text-center">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/manni-liu-66b911304/"><img src={navIcon1} alt="" /></a>
+              <a href="https://github.com/Manni-Liu"><img src={navIcon2} alt="" /></a>
+              <a href="https://medium.com/@manniliu08"><img src={navIcon3} alt="" /></a>
             </div>
             <p>© 2025 Manni Liu. All rights reserved.</p>
           </Col>
